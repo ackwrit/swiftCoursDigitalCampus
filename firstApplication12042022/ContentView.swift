2e364c1b4831d88d370557b7da53b783273e28b2
djinodissingar <dissingardjino@icloud.com>
2022-04-12 09:53:06 +0200

    Initial Commit

---
+++ firstApplication12042022/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstApplication12042022
+//
+//  Created by Djino Dissingar on 12/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firstApplication12042022/firstApplication12042022App.swift
@@ -0,0 +1,17 @@
+//
+//  firstApplication12042022App.swift
+//  firstApplication12042022
+//
+//  Created by Djino Dissingar on 12/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct firstApplication12042022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
